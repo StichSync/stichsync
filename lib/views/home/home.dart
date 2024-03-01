cf5f8413307f3518b50b65b4c214f36a7d883c76
Michał Grabka <mr.grabka@gmail.com>
2024-03-01 17:29:17 +0100

    fix nav btn

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -59,6 +59,7 @@ class _HomeState extends State<Home> {
     return RequireAuthenticated(
       child: Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: const Padding(
             padding: EdgeInsets.only(
               left: 8.0,
