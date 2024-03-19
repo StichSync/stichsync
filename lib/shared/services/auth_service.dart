77d188b21a60e69305dd4684406737c9712387f2
Tomasz Ocłoń <145844356+lasgra@users.noreply.github.com>
2024-03-19 14:37:19 +0100

    added files

---
+++ lib/shared/services/auth_service.dart
@@ -0,0 +1,131 @@
+import 'package:jwt_decode/jwt_decode.dart';
+import 'package:stichsync/shared/components/toaster.dart';
+import 'package:stichsync/shared/models/context/user_claims_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final _client = Supabase.instance.client.auth;
+
+  // getters
+  // todo: refresh session on demand
+  Session? get session => _client.currentSession;
+  bool get isAuthenticated => session != null && !session!.isExpired;
+  UserClaims? get claims {
+    if (!isAuthenticated) return null;
+    final tokenClaims = Jwt.parseJwt(session!.accessToken);
+    return UserClaims(tokenClaims: tokenClaims);
+  }
+
+  Future<bool> register(
+    email,
+    password,
+    username,
+  ) async {
+    try {
+      await _client.signUp(
+        email: email,
+        password: password,
+        data: {
+          'username': username,
+        },
+      );
+      Toaster.toast(msg: "Account created, Please log in", type: ToastType.success, longTime: true);
+      return true;
+    } on AuthException catch (error) {
+      Toaster.toast(
+        msg: error.message,
+        type: ToastType.error,
+        longTime: true,
+      );
+      return false;
+    }
+  }
+
+  Future<bool> login(email, password) async {
+    try {
+      await _client.signInWithPassword(
+        email: email,
+        password: password,
+      );
+      Toaster.toast(
+        msg: "Successfully logged in",
+        type: ToastType.success,
+        longTime: true,
+      );
+      return true;
+    } on AuthException catch (error) {
+      Toaster.toast(
+        msg: error.message,
+        type: ToastType.error,
+        longTime: true,
+      );
+      return false;
+    }
+  }
+
+  Future<bool> forgotPassword(email) async {
+    try {
+      await _client.resetPasswordForEmail(
+        email,
+        redirectTo: "http://localhost:63171/password-reset", // ?
+      );
+      Toaster.toast(
+        msg: "Email with password reset had been sent",
+        type: ToastType.success,
+      );
+    } on AuthException catch (error) {
+      Toaster.toast(
+        msg: error.message,
+        type: ToastType.error,
+      );
+    } catch (error) {
+      Toaster.toast(
+        msg: error.toString(),
+        type: ToastType.error,
+      );
+    }
+    return true;
+  }
+
+  Future<bool> resetPassword(password) async {
+    List<String> urlSplit = Uri.base.toString().split("/");
+    urlSplit.removeLast();
+    try {
+      await _client.updateUser(
+        UserAttributes(password: password),
+      );
+      Toaster.toast(
+        msg: "Password had been reset",
+        type: ToastType.success,
+      );
+
+      return true;
+    } on AuthException catch (error) {
+      Toaster.toast(
+        msg: error.message,
+        type: ToastType.error,
+      );
+    } catch (error) {
+      Toaster.toast(
+        msg: error.toString(),
+        type: ToastType.error,
+      );
+    }
+    return false;
+  }
+
+  Future<bool> logout() async {
+    try {
+      await _client.signOut();
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  Future<Session> refreshSession() async {
+    var refreshed = await _client.refreshSession();
+    if (refreshed.session == null) logout();
+    return refreshed.session!;
+  }
+}
