5453575e7f193e7ec66811cd055e78434bfca24b
lasgra <tomekoclon1@gmail.com>
2024-04-07 16:08:25 +0200

    some changes idk

--- lib/shared/services/account_service.dart
+++ lib/shared/services/account_service.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:stichsync/shared/components/toaster.dart';
 import 'package:stichsync/shared/models/user_profile_model.dart';
-import 'package:stichsync/views/home/inspirations/components/inspiration_post.dart';
 import 'package:stichsync/views/home/inspirations/data_access/inspirations_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class AccountService {
   late final SupabaseClient supabase;
@@ -18,19 +19,21 @@ class AccountService {
     return supabase.auth.currentUser?.id ?? '';
   }
 
+  Future<String> getUserNameFromId(String id) async {
+    var response = await supabase.from('userProfile').select('username').eq("Id", id);
+    return response[0]["username"];
+  }
+
   Future<UserProfileModel> getUserData() async {
     var userId = getUserId();
     var response = await supabase.from('UserProfile').select('username, email, picUrl').eq('userId', userId);
+    var projects = crochetService.getProjectsFromData(await supabase.from('Project').select().eq('userId', userId));
 
     return UserProfileModel(
       email: response[0]["email"],
       username: response[0]["username"],
       picUrl: response[0]["picUrl"],
-      posts: [
-        InspirationPost(crochet: crochetService.get(1, 1)[0]),
-        InspirationPost(crochet: crochetService.get(1, 1)[0]),
-        InspirationPost(crochet: crochetService.get(1, 1)[0]),
-      ],
+      posts: projects,
     );
   }
 
@@ -53,7 +56,7 @@ class AccountService {
       );
       return true;
     } catch (e) {
-      Toaster.toast(
+      SsToaster.toast(
           msg: "We couldn't update your email. Make sure you have stable internet connection and try again.",
           type: ToastType.error);
       return false;
@@ -63,22 +66,42 @@ class AccountService {
   Future<bool> setAvatar(File avatarFile) async {
     try {
       var userId = getUserId();
-      await supabase.storage.from('avatars').update(
-            '$userId/avatar.jpg',
-            avatarFile,
-            fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
-          );
+      if (!kIsWeb) {
+        await supabase.storage.from('avatars').update(
+              '$userId/avatar.jpg',
+              avatarFile,
+              fileOptions: const FileOptions(cacheControl: '3600', upsert: true),
+            );
+      } else {
+        XFile file = XFile(avatarFile.path);
+        await supabase.storage.from('avatars').updateBinary(
+              '$userId/avatar.jpg',
+              await file.readAsBytes(),
+              fileOptions: const FileOptions(cacheControl: '3600', upsert: true),
+            );
+      }
+      await supabase.from('UserProfile').update({
+        'picUrl': 'https://iaxqejougvvfhxqhpmre.supabase.co/storage/v1/object/public/avatars/$userId/avatar.jpg'
+      }).match({'userId': userId});
       return true;
     } on StorageException {
       try {
         var userId = getUserId();
 
-        await supabase.storage.from('avatars').upload(
-              '$userId/avatar.jpg',
-              avatarFile,
-              fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
-            );
-
+        if (!kIsWeb) {
+          await supabase.storage.from('avatars').upload(
+                '$userId/avatar.jpg',
+                avatarFile,
+                fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
+              );
+        } else {
+          XFile file = XFile(avatarFile.path);
+          await supabase.storage.from('avatars').uploadBinary(
+                '$userId/avatar.jpg',
+                await file.readAsBytes(),
+                fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
+              );
+        }
         await supabase.from('UserProfile').update({
           'picUrl': 'https://iaxqejougvvfhxqhpmre.supabase.co/storage/v1/object/public/avatars/$userId/avatar.jpg'
         }).match({'userId': userId});
